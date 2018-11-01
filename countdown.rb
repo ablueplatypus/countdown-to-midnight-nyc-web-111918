c417aa9f3a73e522c913a7f32c3e47b955e5c0a1
Matt Doering <mattdoering86@gmail.com>
2018-11-01 13:20:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,13 +3,8 @@
 def countdown(num)
   count = 0
   while num > count
-    if num == 1
-      puts "#{num} SECOND!!"
-    else
-      puts "#{num} SECONDS!"
-    end
+    puts "#{num} SECOND(S)!"
     num -= 1
   end
   "HAPPY NEW YEAR!"
 end
-countdown(10)
