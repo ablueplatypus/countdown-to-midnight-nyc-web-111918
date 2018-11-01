1508c2f8c3ad231233d84d33bc239f27d1116b31
Matt Doering <mattdoering86@gmail.com>
2018-11-01 13:19:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  count = 0
+  while num > count
+    if num == 1
+      puts "#{num} SECOND!!"
+    else
+      puts "#{num} SECONDS!"
+    end
+    num -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+countdown(10)
